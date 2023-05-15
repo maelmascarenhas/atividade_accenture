1ac55efd9d553c713d4f9612201d8c75dd6f7d85
maelmascarenhas <103335475+maelmascarenhas@users.noreply.github.com>
2023-05-15 05:17:29 -0300

    Update main.dart
    Essa está "completa"

--- main.dart
+++ main.dart
@@ -1,19 +1,26 @@
 import 'package:flutter/material.dart';
-import 'package:todo_list/lib/pages/todo_list_page.dart';
-void main() {  
+import 'package:todo_list/pages/todo_list_page.dart';
+
+void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-  
-  @override  
+
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: const TodoListPage(), 
+    return  MaterialApp(
+      home: TodoListPage()
     );
-  }//O método materialApp está chamando a TodoListPage.
-}//*A página inicialmente é Stateless*
+  } //O método materialApp está chamando a TodoListPage.
+}
+
+  @override
+  Widget build(BuildContext context) {
+    return TodoListPage();
+  }
+//*A página inicialmente é Stateless*
 //Os widgets Stateless são úteis para componentes da interface do usuário 
 //que não precisam armazenar nenhum estado interno, como botões, 
 //ícones e outros elementos que não precisam mudar dinamicamente. 
@@ -21,4 +28,4 @@ class MyApp extends StatelessWidget {
 //os widgets Stateless são mais simples e eficientes em termos de desempenho.
 //Em resumo,os *widgets Stateful* são úteis quando você precisa de um widget  
 //que possa mudar dinamicamente em resposta a eventos ou dados externos.
-
+ 
