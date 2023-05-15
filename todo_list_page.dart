d7d376065ae7975917ea85875d21e221a1776dba
maelmascarenhas <103335475+maelmascarenhas@users.noreply.github.com>
2023-05-15 05:15:12 -0300

    Update todo_list_page.dart
    O código ainda está quebrado só falta colocar as funcionalidades nos botões

--- todo_list_page.dart
+++ todo_list_page.dart
@@ -1,25 +1,103 @@
 import 'package:flutter/material.dart';
 
-class TodoListPage extends StatelessWidget {
-  const TodoListPage({Key? key}) : super(key: key); 
-    
-  @override 
-  Widget build(BuildContext context){    
-    return const Scaffold(
+class TodoListPage extends StatefulWidget {
+  TodoListPage({Key? key}) : super(key: key);
+
+  @override
+  State<TodoListPage> createState() => _TodoListPageState();
+}
+
+class _TodoListPageState extends State<TodoListPage> {
+  final TextEditingController todoController = TextEditingController();
+
+  List<String> todos = [];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
       body: Center(
         child: Padding(
-          padding: EdgeInsets.only(left: 16, right: 16),
-          child: TextField(
-            decoration: InputDecoration(//como se fosse o botão de enviar do instagram
-                icon: Icon(Icons.send),
-                hintText: 'Coloque seu nome Aqui',
-                helperText: 'Helper Text',
-                counterText: '0 characters',
-                border: OutlineInputBorder(),
-            ), 
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      controller: todoController,
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Adicione uma tarefa',
+                        hintText: 'Estudar Flutter',
+                      ),
+                    ),
+                  ),
+                  const SizedBox(width: 8),
+                  ElevatedButton(
+                      onPressed: () {
+                        String text = todoController.text;
+                        setState(() {
+                          todos.add(text);
+                        });
+                      },
+                      style: ElevatedButton.styleFrom(
+                          primary: Color(0xff00d7f3),
+                          padding: EdgeInsets.all(14),
+                      ),
+                      child: const Icon(
+                        Icons.add,
+                        size: 30,
+                      ),
+                  ),
+                ],
+              ),
+              Row(
+                children: [ 
+                  const SizedBox(height: 16),
+              ListView(
+                shrinkWrap: true,
+                children: [
+                  for(String todo in todos)
+                    ListTile(
+                      title: Text(todo),
+                    ),
+                ],
+              ),
+              SizedBox(height: 16),
+              Row(
+                children: [
+                  Expanded(
+                      child: Text(
+                          'Você possui 0 trefas pensentes'
+                      )
+                  ),
+                  const SizedBox(width: 8),
+                  ElevatedButton(
+                      onPressed: () {
+                        String text = todoController.text;
+                          setState(() {
+                            todoController.clear();
+                          }
+                        );
+                      },
+                      style: ElevatedButton.styleFrom(
+                        primary: Color(0xff00d7f3),
+                        padding: EdgeInsets.all(14)
+                      ),
+                      child: Text('Limpar Tudo'),
+
+                  ),
+
+                ],
+              ),
+            ],
           ),
-        ),
+        ]),
       ),
-    );
+    ));
   }
 }
+
+
+
